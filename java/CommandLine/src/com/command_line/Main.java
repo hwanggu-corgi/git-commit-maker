30dacdbe5a38a319f4fe2bb0a9383649af4789e8
Hyungmo Gu <guhyungm7@gmail.com>
2022-06-27 23:20:23 -0600

    Add files

---
+++ java/CommandLine/src/com/command_line/Main.java
@@ -0,0 +1,33 @@
+package com.command_line;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.util.concurrent.Callable;
+
+@Command(name="gitchat", mixinStandardHelpOptions= true, version = "gitchat 1.0", description = "Sends perpetual commit request and texts to server.")
+public class Main implements Callable<Integer> {
+
+    @Option(names = {"-cl", "--cool-down"}, description = "Cool down amount after commit (in seconds)")
+    private int coolDownAmt = 0;
+
+    @Option(names = {"-c", "--commit"}, description = "Commit amount per interval")
+    private int commitAmt = 0;
+
+    @Option(names = {"-m", "--message"}, description = "Message to add")
+    private String message = "";
+
+    public Integer call() throws Exception {
+        System.out.println("coolDownAmt: " + coolDownAmt);
+        System.out.println("commitAmt: " + commitAmt);
+        System.out.println("message: " + message);
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new Main()).execute(args);
+        System.exit(exitCode);
+    }
+}
